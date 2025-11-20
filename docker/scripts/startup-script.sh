745c9901f34353a6d2ac95b75be8c70fc3faa207
amir2520 <amir2520@gmail.com>
2025-11-19 23:50:45 -0600

    modified startup ssh tunneling

--- docker/scripts/startup-script.sh
+++ docker/scripts/startup-script.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# set -o errexit
-# set -o pipefail
-# set -o nounset
-
-# if [[ ${IS_PROD_ENV} == 'true' ]]; then
-# 	/usr/local/gcloud/google-cloud-sdk/bin/gcloud compute ssh "${VM_NAME}" --zone "${ZONE}" --tunnel-through-iap -- -N -L "${PROD_MLFLOW_SERVER_PORT}:localhost:${PROD_MLFLOW_SERVER_PORT}"
-# else
-# 	/start-tracking-server.sh &
-# 	tail -F anything 
-# fi 	
-
-
-
-
 set -o errexit
 set -o pipefail
 set -o nounset
